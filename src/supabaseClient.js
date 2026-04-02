157c49cc35c5cb47ed6b856d5afbb647d9030ae9
William Baldwin <williambaldwin@mac.myfiosgateway.com>
2026-04-01 20:30:07 -0400

    Fix: update supabaseClient with correct anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://moyhcebdltdnfxdbbxvs.supabase.co",
-  "sb_publishable_0q60uasG4FBzQbxNnqO8Yw_PTRYJNZO"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1veWhjZWJkbHRkbmZ4ZGJieHZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMTY2MTcsImV4cCI6MjA4ODU5MjYxN30.55h8i9oryvC8QeGz8pK12pYrVZhncIC6qPSYbWrt8Pc"
 );
 
 export default supabase;
